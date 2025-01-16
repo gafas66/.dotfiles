e6f58ceb1fcf97175cbf5418ecac3d62c7dcde51
ESK <ekofoed@gmail.com>
2025-01-16 22:01:50 +0000

    Reworked init to work properly with debian xterm/uxterm/terminal

--- init.el
+++ init.el
@@ -2,6 +2,12 @@
 ; Emacs init file
 ;--------------------------------------------------------------------------------
 
+; Lines below because settings.org contains eval:
+(setq custom-file (locate-user-emacs-file "custom-vars.el"))
+(load custom-file)
+;(add-hook 'kill-emacs-query-functions
+;          'custom-prompt-customize-unsaved-options)
+
 (org-babel-load-file "~/.dotfiles/settings.org")
 
 ; End of file

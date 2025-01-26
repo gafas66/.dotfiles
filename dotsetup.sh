37328fd62d67c4b01cd8fc08094aae89f2488b68
ESK <ekofoed@gmail.com>
2025-01-26 16:47:49 +0000

    Fixed so not using touch

--- dotsetup.sh
+++ dotsetup.sh
@@ -10,6 +10,6 @@ gpg -d ~/.dotfiles/.alias_local.asc > ~/.alias_local
 mkdir ~/.emacs.d
 ln -s ~/.dotfiles/init.el ~/.emacs.d/.
 ln -s ~/.dotfiles/lisp ~/.emacs.d/.
-touch ~/.emcacs.d/custom-vars.el
+cat /dev/null > ~/.emcacs.d/custom-vars.el
 
 echo "NB! ~/init needs to be cloned as org files are there"

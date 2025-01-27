3a8b9f6f66ff797a53011f759fff14a66531e2e0
ESK <ekofoed@gmail.com>
2025-01-27 01:14:45 +0000

    Added early init file for straight

--- dotsetup.sh
+++ dotsetup.sh
@@ -9,6 +9,7 @@ gpg -d ~/.dotfiles/.alias_local.asc > ~/.alias_local
 
 mkdir ~/.emacs.d
 ln -s ~/.dotfiles/init.el ~/.emacs.d/.
+ln -s ~/.dotfiles/early-init.el ~/.emacs.d/.
 ln -s ~/.dotfiles/lisp ~/.emacs.d/.
 echo "" > ~/.emacs.d/custom-vars.el
 
---
+++ early-init.el
@@ -0,0 +1,20 @@
+;;------------------------------------------------------------------------------
+;; Created: Monday, January 27 2025
+;; Author: Erik Kofoed, ESK
+;; Description:
+;; 
+
+;;; Code:
+
+;; Disable package.el in favour of straight.el
+(setq package-enable-at-startup nil)
+
+(provide 'early-init)
+;;; early-init.el ends here
+
+;; End of file
+;;----------------------------------------------------------------------------------------------------------------------
+;; Local Variables:
+;; comment-column: 60
+;; End:
+;;----------------------------------------------------------------------------------------------------------------------

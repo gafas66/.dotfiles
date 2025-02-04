15228a41282e82c0cac361a54a1ca764158cc023
ESK <ekofoed@gmail.com>
2025-02-04 00:48:58 +0000

    Improved lsp?

--- debian.sh
+++ debian.sh
@@ -2,9 +2,15 @@
 # This is base install for debian VM
 
 # Not always default
-apt -y install git gitk emacs zip unzip tree htop ruby
+apt -y install git gitk emacs zip unzip tree htop ruby ripgrep
 
-# Needed for installing guest additions on vbox
+sudo bash < <(curl -s https://raw.githubusercontent.com/clojure-lsp/clojure-lsp/master/install)
+
+curl -sLO https://raw.githubusercontent.com/babashka/babashka/master/install
+chmod +x install
+./install
+
+ # Needed for installing guest additions on vbox
 apt -y install build-essential dkms linux-headers-$(uname -r)
 
 # then open graphic file browser to click on guest-add cdrom to mount it
